37083de1d21ed09128c7fa3969968e4b6a17f72f
aayush18602 <aayushpandey18602@gmail.com>
2025-09-27 15:22:44 +0530

    Added backend initial setup

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.set("io", io);
+
+app.get('/',(req,res)=>{
+    res.send("Hello world")
+})
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
